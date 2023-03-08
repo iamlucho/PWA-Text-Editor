7e3b941e96d98a6c50b047ad6575519cae8122ab
Luciano Aguirre <luciano.aguirre@collins.com>
2023-03-08 14:26:47 -0500

    Added logic to handle assets

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  /\.(js|css|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+  new CacheFirst({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 100,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
